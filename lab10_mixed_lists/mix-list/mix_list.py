dec10ec66575ceecba77fd1747a67770aa4ae7f2
Rebellia <sleepy.wrath@gmail.com>
2018-12-11 13:38:32 -0500

    completed lab 10

---
+++ lab10_mixed_lists/mix-list/mix_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+myList = ["string", 5060, "12.43"]
+
+print("The first item in the list: " + myList[0])
+
+print("The second item in the list: " + str(myList[1]))
+
+print("The last item in the list: " + myList[2])
