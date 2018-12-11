06859aa5dc9f3f215eed917e1074745ee78ca891
Rebellia <sleepy.wrath@gmail.com>
2018-12-11 18:07:37 -0500

    added some more stuff to lab 11

--- lab11_more_lists/mix_list2/mix-list2.py
+++ lab11_more_lists/mix_list2/mix-list2.py
@@ -3,20 +3,27 @@
 protocols = ['ssh', 'http', 'https']
 protocols2 = ['ssh', 'http', 'https']
 
+print("protocols is: ", protocols)
 
-print(protocols)
+print("The first item in protocols is: ", protocols[1])
+
+protocols.extend('dns')
+
+print("protocols after extending dns is: ", protocols)
 
 protocols.append('dns')
 protocols2.append('dns')
 
-print(protocols)
+print("protocols after appending dns is: ", protocols)
 
 proto3 = [ 22, 80, 443, 53]
 
+#extend will put the individual list items inside of protocols
 protocols.extend(proto3)
 
-print(protocols)
+print("protocols after extending proto3 is: ", protocols)
 
+#append will put the list itself as an individual list item
 protocols2.append(proto3)
 
-print(protocols2)
+print("protocols2 after appending dns and proto3 is: ", protocols2)
