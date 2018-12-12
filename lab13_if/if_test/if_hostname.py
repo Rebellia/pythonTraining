546f9cb330ffe21a73743e41bdcaecef3e5cf003
Rebellia <sleepy.wrath@gmail.com>
2018-12-12 15:51:46 -0500

    Completed lab 13

---
+++ lab13_if/if_test/if_hostname.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+hostname = input('What is the hostname? ')
+
+if hostname.lower() == 'mtg':
+   print('The hostname was found to be mtg')
+   print('The hostname mathes the expected config')
+
+
+print('Exiting the script')
